992c6f5de74bccc5471ed52cb8349f83f0ac62f6
yoan yomba <yoanyombapro@gmail.com>
2021-03-24 13:15:52 -0400

    feat: setup frontend
    Signed-off-by: yoan yomba <yoanyombapro@gmail.com>

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,23 +1,12 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import {Route, Switch} from 'react-router-dom';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import Header from './components/header/header.component';
-import HomePage from './pages/homepage/homepage.component';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-          <Header />
-          <Switch>
-            <Route exact path='/'  component={HomePage} />
-            <Route path='/signin'  component={SignInAndSignUpPage} />
-          </Switch>
-      
-      </div>
-    );
-  }
+const App = () => {
+  return (
+		<>
+		<h1> FeelGuuds </h1>
+    </>
+  );
 }
 
 export default App;
