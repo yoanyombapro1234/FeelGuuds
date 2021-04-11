42f4f6ca3b15540dccc91128b295fd5d953179bf
yoan yomba <yoanyombapro@gmail.com>
2021-04-10 23:25:52 -0400

    feat: enhance deployment file
    Signed-off-by: yoan yomba <yoanyombapro@gmail.com>

--- src/services/authentication_service/test/build.sh
+++ src/services/authentication_service/test/build.sh
@@ -4,4 +4,4 @@ set -e
 
 # build the docker file
 GIT_COMMIT=$(git rev-list -1 HEAD) && \
-DOCKER_BUILDKIT=1 docker pull keratin/authn-server:latest
+DOCKER_BUILDKIT=1 docker pull keratin/authn-server:1.10.4
