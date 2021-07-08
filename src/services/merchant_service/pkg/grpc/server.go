72a5cb88c53a4e027c5cd95052f0b337e1cbe332
yoanyombapro1234 <yoanyombapro@gmail.com>
2021-07-08 15:06:24 -0400

    feat: configure grpc server
    Signed-off-by: yoanyombapro1234 <yoanyombapro@gmail.com>

--- src/services/merchant_service/pkg/grpc/server.go
+++ src/services/merchant_service/pkg/grpc/server.go
@@ -10,9 +10,8 @@ import (
 	grpc_opentracing "github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
 	otgrpc "github.com/opentracing-contrib/go-grpc"
 	core_logging "github.com/yoanyombapro1234/FeelGuuds/src/libraries/core/core-logging/json"
-	core_metrics "github.com/yoanyombapro1234/FeelGuuds/src/libraries/core/core-metrics"
 	core_tracing "github.com/yoanyombapro1234/FeelGuuds/src/libraries/core/core-tracing"
-	"go.uber.org/zap"
+	"github.com/yoanyombapro1234/FeelGuuds/src/services/merchant_service/gen/github.com/yoanyombapro1234/FeelGuuds/src/merchant_service/proto/merchant_service_proto_v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
@@ -20,21 +19,28 @@ import (
 )
 
 type Server struct {
+	// inherit the behaviors/adhere to the interface the api server adheres to
 	config        *Config
 	logger        core_logging.ILog
-	metricsEngine *core_metrics.CoreMetricsEngine
 	tracerEngine  *core_tracing.TracingEngine
 }
+
+var _ merchant_service_proto_v1.MerchantServiceServer = (*Server)(nil)
 
 type Config struct {
 	Port        int    `mapstructure:"GRPC_PORT"`
 	ServiceName string `mapstructure:"GRPC_SERVICE_NAME"`
+	RpcDeadline     int    `mapstructure:"grpc-rpc-deadline"`
+	RpcRetries      int    `mapstructure:"grpc-rpc-retries"`
+	RpcRetryTimeout int    `mapstructure:"grpc-rpc-retry-timeout"`
+	RpcRetryBackoff int    `mapstructure:"grpc-rpc-retry-backoff"`
 }
 
-func NewServer(config *Config, logger *zap.Logger) (*Server, error) {
+func NewServer(config *Config, logging core_logging.ILog, tracer *core_tracing.TracingEngine) (*Server, error) {
 	srv := &Server{
-		logger: logger,
+		logger: logging,
 		config: config,
+		tracerEngine: tracer,
 	}
 
 	return srv, nil
@@ -43,7 +49,8 @@ func NewServer(config *Config, logger *zap.Logger) (*Server, error) {
 func (s *Server) ListenAndServe() {
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%v", s.config.Port))
 	if err != nil {
-		s.logger.Fatal("failed to listen", zap.Int("port", s.config.Port))
+		var msg = fmt.Sprintf("faled to listen on port %d", s.config.Port)
+		s.logger.Fatal(err, msg)
 	}
 
 	// configure tracing so all future rpc activity will be traced by use of s
@@ -63,10 +70,14 @@ func (s *Server) ListenAndServe() {
 
 	server := health.NewServer()
 	reflection.Register(srv)
+
+	// use auto generate code to register server
+	merchant_service_proto_v1.RegisterMerchantServiceServer(srv, s)
 	grpc_health_v1.RegisterHealthServer(srv, server)
 	server.SetServingStatus(s.config.ServiceName, grpc_health_v1.HealthCheckResponse_SERVING)
 
 	if err := srv.Serve(listener); err != nil {
-		s.logger.Fatal("failed to serve", zap.Error(err))
+		var msg = fmt.Sprintf("faled to serve on port %d", s.config.Port)
+		s.logger.Fatal(err, msg)
 	}
 }
